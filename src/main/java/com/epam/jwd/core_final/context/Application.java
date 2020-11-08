bb0409dfe60dffad556918e8be24a7ecb0527e4a
Vladislav Churyla <tagirovicv@gmail.com>
2020-11-09 00:20:06 +0300

    implemented ApplicationContext

--- src/main/java/com/epam/jwd/core_final/context/Application.java
+++ src/main/java/com/epam/jwd/core_final/context/Application.java
@@ -1,16 +1,15 @@
 package com.epam.jwd.core_final.context;
 
 import com.epam.jwd.core_final.context.impl.NassaContext;
-import com.epam.jwd.core_final.exception.InvalidStateException;
 
 import java.util.function.Supplier;
 
 public interface Application {
 
-    static ApplicationMenu start() throws InvalidStateException {
-        final Supplier<ApplicationContext> applicationContextSupplier = null; // todo
-        final NassaContext nassaContext = new NassaContext();
-
+    static ApplicationMenu start() throws Exception {
+        final Supplier<ApplicationContext> applicationContextSupplier = NassaContext::getInstance;
+        final NassaContext nassaContext = NassaContext.getInstance();
+        
         nassaContext.init();
         return applicationContextSupplier::get;
     }
