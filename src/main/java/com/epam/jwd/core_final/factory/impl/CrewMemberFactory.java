78fd55410f2c73ef12ce3bd380202fbff711fb7b
Vladislav Churyla <tagirovicv@gmail.com>
2020-11-09 00:16:40 +0300

    implemented CrewMemberFactory

--- src/main/java/com/epam/jwd/core_final/factory/impl/CrewMemberFactory.java
+++ src/main/java/com/epam/jwd/core_final/factory/impl/CrewMemberFactory.java
@@ -1,13 +1,26 @@
 package com.epam.jwd.core_final.factory.impl;
 
 import com.epam.jwd.core_final.domain.CrewMember;
+import com.epam.jwd.core_final.domain.Rank;
+import com.epam.jwd.core_final.domain.Role;
 import com.epam.jwd.core_final.factory.EntityFactory;
 
 // do the same for other entities
 public class CrewMemberFactory implements EntityFactory<CrewMember> {
+    private static final CrewMemberFactory instance = new CrewMemberFactory();
+
+    private CrewMemberFactory(){
+    }
+
+    public static CrewMemberFactory getInstance() {
+        return instance;
+    }
 
     @Override
     public CrewMember create(Object... args) {
-        return null;
+        String name = (String) args[0];
+        Role role = (Role) args[1];
+        Rank rank = (Rank) args[2];
+        return new CrewMember(name, role, rank);
     }
 }
