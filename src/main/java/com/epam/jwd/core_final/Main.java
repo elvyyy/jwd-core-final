b83143dadd6f2fe8169480ab3b433d375be44b67
Vladislav Churyla <tagirovicv@gmail.com>
2020-11-09 00:18:34 +0300

    Main, exceptions

--- src/main/java/com/epam/jwd/core_final/Main.java
+++ src/main/java/com/epam/jwd/core_final/Main.java
@@ -1,10 +1,24 @@
 package com.epam.jwd.core_final;
 
 import com.epam.jwd.core_final.context.Application;
+import com.epam.jwd.core_final.context.ApplicationMenu;
+import com.epam.jwd.core_final.context.impl.NassaContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        Application.start();
+        log.info("Starting application...");
+        NassaContext instance;
+        try {
+            ApplicationMenu start = Application.start();
+            instance = NassaContext.getInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        log.info("Finishing execution.");
     }
 }
--- src/main/java/com/epam/jwd/core_final/exception/UnknownEntityException.java
+++ src/main/java/com/epam/jwd/core_final/exception/UnknownEntityException.java
@@ -21,6 +21,7 @@ public class UnknownEntityException extends RuntimeException {
     public String getMessage() {
         // todo
         // you should use entityName, args (if necessary)
+
         return null;
     }
 }
