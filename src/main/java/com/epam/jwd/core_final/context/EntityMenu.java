cb4c27750f8d22baa04879231415440ffc2a4255
Vladislav Churyla <tagirovicv@gmail.com>
2020-11-11 23:13:54 +0300

    MainMenu interface

---
+++ src/main/java/com/epam/jwd/core_final/context/EntityMenu.java
@@ -0,0 +1,29 @@
+package com.epam.jwd.core_final.context;
+
+import java.util.Scanner;
+
+public interface EntityMenu {
+
+    default Object show(Object o) {
+        boolean notOk = true;
+        int choice = -1;
+        while (notOk) {
+            System.out.println("1 - crewMembers");
+            System.out.println("2 - spaceships");
+            System.out.println("3 - missions");
+            Scanner scanner = new Scanner(System.in);
+            try {
+                choice = Integer.valueOf(scanner.nextLine());
+                if (choice < 1 || choice > 3) {
+                    continue;
+                }
+                notOk = false;
+            } catch (Exception e) {
+                System.out.println("Oops... Something went wrong :(");
+            }
+        }
+        return choice;
+    }
+
+    Object handleInput(Object o);
+}
