9280526bcf6edc95117ba109d68783890815abcd
Vladislav Churyla <tagirovicv@gmail.com>
2020-11-11 23:11:32 +0300

    Added InvalidStateException

--- src/main/java/com/epam/jwd/core_final/exception/InvalidStateException.java
+++ src/main/java/com/epam/jwd/core_final/exception/InvalidStateException.java
@@ -9,4 +9,21 @@ public class InvalidStateException extends Exception {
     public InvalidStateException(String message) {
         super(message);
     }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    public InvalidStateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidStateException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidStateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
